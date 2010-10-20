d65103794d04c1bffe3c180787c94a0ab7e3f3ed
Austin Robert Bales <austin@odopod.com>
2010-10-19 18:04:34 -0700

    Keychain stuff.

--- LoginSheetController.rb
+++ LoginSheetController.rb
@@ -5,19 +5,39 @@
 # Copyright 2010 __MyCompanyName__. All rights reserved.
 
 class LoginSheetController < BWSheetController
-	attr_accessor :password_field, :username_field, :wheel, :warning
+	attr_accessor :password_field, :username_field, :wheel, :warning, :delegator
+	
+	def awakeFromNib()
+	
+		standardUserDefaults = NSUserDefaults.standardUserDefaults
+
+		if (standardUserDefaults)
+			username = standardUserDefaults.objectForKey "RamblerUsername"
+			keyitem = EMGenericKeychainItem.genericKeychainItemForService "Rambler", withUsername: username
+			puts keyitem.password
+		end
+	
+	end
 		
 	def do_login(sender)
+	
+			standardUserDefaults = NSUserDefaults.standardUserDefaults
+
+			if (standardUserDefaults)
+				standardUserDefaults.setObject @username_field.stringValue(), forKey:"RamblerUsername"
+				standardUserDefaults.synchronize
+			end
+	
 			@warning.setHidden(true)
 			@wheel.startAnimation(nil)
 			@password_field.setEnabled false
 			@username_field.setEnabled false
-			
+#X			referencer = self;
 			MacRubyHTTP.post("http://desk.austinbales.com/login", {payload: {username: @username_field.stringValue, password: @password_field.stringValue}}) do |response|
 				body = NSString.alloc.initWithData(response.body, encoding:NSString.defaultCStringEncoding)
 				if (body == "Success")
-					self.closeSheet(nil)
-					return true
+					EMGenericKeychainItem.addGenericKeychainItemForService "Rambler", withUsername:@username_field.stringValue, password:@password_field.stringValue()
+					@delegator.closeSheet(nil)
 				else
 					@wheel.stopAnimation(nil)
 					@username_field.setEnabled true
@@ -26,10 +46,6 @@ class LoginSheetController < BWSheetController
 				end
 			end
 		
-	end
-	
-	def closeSheet(sender)
-		puts "closeSheet"
 	end
 end
 
